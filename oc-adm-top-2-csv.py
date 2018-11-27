677c3497c95e3ed6e1ef81ffdc2f069a55bcb411
Christopher Markieta <markietachristopher@gmail.com>
2018-11-27 10:40:28 -0500

    Working output CPU CSV

--- oc-adm-top-2-csv.py
+++ oc-adm-top-2-csv.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import argparse
+import csv
 
 def main():
     parser = argparse.ArgumentParser(description='Convert output of oc-adm-top'
@@ -8,9 +9,29 @@ def main():
     parser.add_argument('-f', '--files', type=argparse.FileType('r'),
                         nargs='+', required=True)
     args = parser.parse_args()
-    for f in args.files:
-        for line in f:
-            print line
+    
+    files = {}
 
-if __name__ == "__main__":
+    for f in args.files:
+        files[f] = {}
+        for line in f:
+            namespace, pod, cpu, memory = line.split()
+            if namespace not in files[f]: files[f][namespace] = {}
+            files[f][namespace][pod] = {}
+            files[f][namespace][pod]['cpu'] = cpu
+            files[f][namespace][pod]['memory'] = memory
+    
+    with open('namespace-cpu.csv', 'wb') as csv_file:
+        writer = csv.writer(csv_file, delimiter=',')
+
+        for f in files:
+            for namespace, pods in files[f].items():
+                cpu = [namespace]
+                memory = [namespace]
+                for pod, properties in pods.items():
+                    cpu.append(properties['cpu'])
+                    memory.append(properties['cpu'])
+                writer.writerow(cpu)
+
+if __name__ == '__main__':
     main()
